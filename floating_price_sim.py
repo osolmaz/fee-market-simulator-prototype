a247327cad2d18b6e1988c10e3f266dd3969169c
onursolmaz <onursolmaz@gmail.com>
2020-05-27 12:43:55 +0200

    Added bidding strategy for agents

--- floating_price_sim.py
+++ floating_price_sim.py
@@ -14,6 +14,8 @@ from transaction import Transaction, TransactionPool
 BLOCK_GAS_LIMIT = 10_000_000
 TX_GAS_USED = 21_000
 
+OVERBIDDING_RATE = 0.1
+
 # BLOCK_TIME = 13
 BLOCK_TIME = 600
 SECONDS_IN_DAY = 60 * 60 * 24
@@ -32,16 +34,32 @@ def n_user_fun(i):
 txpool = TransactionPool()
 
 X = list(range(N_BLOCKS))
-P = []
+
 n_user_arr = []
-bar = progressbar.ProgressBar(max_value=N_BLOCKS)
 txpool_size_arr = []
+median_price_arr = []
+
+bar = progressbar.ProgressBar(max_value=N_BLOCKS)
 
 for x in X:
     n_user = int(n_user_fun(x))
 
+    if len(median_price_arr) == 0:
+        prev_median_price = 0.1
+    else:
+        prev_median_price = median_price_arr[-1]
+
     # prices = [sample_price() for i in range(n_user)]
-    prices = sample_price(size=n_user)
+    wtp_arr = sample_price(size=n_user)  # Willingness to pay
+    prices = []
+    for wtp in wtp_arr:
+        bid_price = prev_median_price + prev_median_price * OVERBIDDING_RATE
+
+        if bid_price > wtp:
+            prices.append(wtp)
+        else:
+            prices.append(bid_price)
+
     txs = [Transaction(TX_GAS_USED, p) for p in prices]
     txpool.add_txs(txs)
 
@@ -50,7 +68,7 @@ for x in X:
     prices_of_included_txs = [i.gas_price for i in included_txs]
     median_price = np.median(prices_of_included_txs)
 
-    P.append(median_price)
+    median_price_arr.append(median_price)
     n_user_arr.append(n_user)
     txpool_size_arr.append(txpool.get_size())
 
@@ -59,7 +77,7 @@ for x in X:
 bar.finish()
 
 plt.figure()
-plt.plot(X, P)
+plt.plot(X, median_price_arr)
 plt.ylim(bottom=0)
 plt.title("Median gas price")
 plt.grid()
@@ -70,11 +88,11 @@ plt.ylim(bottom=0)
 plt.title("Number of txs sent")
 plt.grid()
 
-plt.figure()
-plt.plot(X, txpool_size_arr)
-plt.ylim(bottom=0)
-plt.title("Size of the tx pool")
-plt.grid()
+# plt.figure()
+# plt.plot(X, txpool_size_arr)
+# plt.ylim(bottom=0)
+# plt.title("Size of the tx pool")
+# plt.grid()
 
 
 plt.show()
--- transaction.py
+++ transaction.py
@@ -8,30 +8,35 @@ class Transaction:
 
 
 class TransactionPool:
-    def __init__(self):
-        self.pool = SortedList(key=lambda t: t.gas_price)
+    def __init__(self, limit=1_000_000):
+        self._pool = SortedList(key=lambda t: t.gas_price)
+        self.limit = limit
 
     def add_txs(self, txs):
-        self.pool.update(txs)
+        self._pool.update(txs)
+
+        if len(self._pool) > self.limit:
+            overflow = len(self._pool) - self.limit
+            del self._pool[:overflow]
 
     def pop_most_valuable_txs(self, total_gas_target=None):
         if total_gas_target is None:
-            return self.pool.pop()
+            return self._pool.pop()
         elif isinstance(total_gas_target, int) and total_gas_target > 0:
             result = []
             while True:
-                if len(self.pool) == 0:
+                if len(self._pool) == 0:
                     break
                 if (
-                    sum(i.gas_used for i in result) + self.pool[-1].gas_used
+                    sum(i.gas_used for i in result) + self._pool[-1].gas_used
                     > total_gas_target
                 ):
                     break
 
-                result.append(self.pool.pop())
+                result.append(self._pool.pop())
             return result
         else:
             raise Exception("Invalid gas target")
 
     def get_size(self):
-        return len(self.pool)
+        return len(self._pool)
